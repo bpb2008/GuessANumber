1fb37e8c6a8ec8b91d15b38a79e62c6dcacd79e4
bpb2008 <brittanythompson08@gmail.com>
2023-11-15 12:33:25 -0600

    consolodated css body styles and deleted unnecessary console.log

--- src/Components/Result.jsx
+++ src/Components/Result.jsx
@@ -2,7 +2,6 @@ import { useRef } from "react";
 
 const Result = ({ userNum }) => {
   const randomNum = useRef(Math.floor(Math.random() * 100) + 1);
-  console.log(randomNum);
 
   if (userNum < 0) {
     return <h3>Enter valid input</h3>;
@@ -12,12 +11,12 @@ const Result = ({ userNum }) => {
 
   if (randomNum.current > userNum) {
     result = "Your guess is too low!";
-  } else if (randomNum.current < userNum) {
+  }
+  if (randomNum.current < userNum) {
     result = "Your guess is too high";
-  } else if (randomNum.current === userNum) {
+  }
+  if (randomNum.current === userNum) {
     result = "Yay! You guessed correctly!";
-  } else {
-    result = "Enter valid input!";
   }
 
   return (
